7e7bfebd348f5f346e6e3fbc631e0a07c4935471
rayhan <rayhan@gmail.com>
2023-04-04 00:31:59 +0600

    Added checkdata, error, and success functions

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ document.querySelector("#submit").addEventListener("click", function (event) {
 })
 
 function checkdata() {
+    if (username.value == "") {
+        error(username, "Username can't be blank");
+    } else {
+        success(username);
+    }
+}
 
+function error(u, msg) {
+    u.parentNode.className = "form error";
+    let icon = document.querySelector(".fa");
+    icon.className = "fa fa-exclamation-circle";
+
+    let span = document.querySelector("span");
+    span.innerText = msg;
+}
+
+function success(u) {
+    u.parentNode.className = "form success";
+    let icon = document.querySelector(".fa");
+    icon.className = "fa fa-check-circle";
 }
