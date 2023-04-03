4d34d261b3422290ce0c4b4b0d1bdf11576df2ac
rayhan <rayhan@gmail.com>
2023-04-04 00:04:26 +0600

    Added two functions inside js file

---
+++ script.js
@@ -0,0 +1,10 @@
+let username = document.querySelector("#username");
+
+document.querySelector("#submit").addEventListener("click", function (event) {
+    event.preventDefault();
+    checkdata();
+})
+
+function checkdata() {
+
+}
